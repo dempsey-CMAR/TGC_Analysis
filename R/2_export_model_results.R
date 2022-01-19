3bc689e295147c7581f681383dda349740789197
Danielle Dempsey <ddempsey@perennia.ca>
2022-01-19 15:59:23 -0400

    export results as rds files to preserve the ordered factors (STATION and DEPTH)

--- R/2_export_model_results.R
+++ R/2_export_model_results.R
@@ -7,6 +7,7 @@ library(here)          # relative files paths
 library(lubridate)
 library(strings)       # convert_depth_to_ordered_factor() function
 library(tgc)           # season functions
+library(tidyr)
 
 # import data
 dat_raw <- import_strings_data(
@@ -14,19 +15,27 @@ dat_raw <- import_strings_data(
   county = c("Halifax", "Guysborough_Dover Bay", "Lunenburg")
 ) %>%
   filter(VARIABLE == "Temperature") %>% 
-  select(-WATERBODY, -LEASE, -VARIABLE, -SENSOR, -DEPLOYMENT_PERIOD, 
+  select(-WATERBODY, -LEASE, -VARIABLE, -DEPLOYMENT_PERIOD, 
          -UNITS, -MOORING) %>% 
   filter(
     STATION == "Flat Island" | 
       STATION == "Beaver Point" | 
       STATION == "Madeline Point"
   ) %>% 
+  # fix Flat Island data (Vemco was at 22 m; remove 20 m HOBO that was only in 2 deployments)
+  separate(SENSOR, into = c("SENSOR", NA), sep = "-") %>% 
+  filter(!(STATION == "Flat Island" & DEPTH == '20' & SENSOR == "HOBO")) %>% 
+  select(-SENSOR) %>% 
+  mutate(
+    DEPTH = if_else(STATION == "Flat Island" & DEPTH == "20", "22", DEPTH)
+  ) %>% 
   convert_depth_to_ordered_factor() %>% 
   mutate(
     STATION = ordered(
       STATION, levels = c("Madeline Point", "Beaver Point", "Flat Island")
     )
-  )
+  ) 
+
 
 # Analysis -------------------------------------------------------------
 
@@ -79,12 +88,25 @@ dat_filt <- dat_seasons %>%
 
 # Export results ----------------------------------------------------------
 
-fwrite(dat_seasons, here("results/dat_seasons.csv"))
-fwrite(gap_table, here("results/gap_table.csv"))
-fwrite(dd, here("results/dd.csv"))
-fwrite(tgc_table, here("results/tgc_table.csv"))
-fwrite(dat_filt, here("results/dat_filt.csv"))
+results <- list(
+  dat_raw = dat_raw,
+  dat_seasons = dat_seasons,
+  dat_filt = dat_filt,
+  gap_table = gap_table,
+  dd = dd,
+  tgc_table = tgc_table
+)
 
+saveRDS(results, here("results/model_results.rds"))
+
+
+# fwrite(dat_raw, here("results/dat_raw.csv"))
+# fwrite(dat_seasons, here("results/dat_seasons.csv"))
+# fwrite(dat_filt, here("results/dat_filt.csv"))
+# 
+# fwrite(gap_table, here("results/gap_table.csv"))
+# fwrite(dd, here("results/dd.csv"))
+# fwrite(tgc_table, here("results/tgc_table.csv"))
 
 
 
